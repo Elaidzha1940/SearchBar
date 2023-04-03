fad6fa27beff59fbecbb80b250ee41489eb2b6cc
Elaidzha1940 <eli080293@gmail.com>
2023-04-03 13:28:40 +0700

    SearchBar

---
+++ SearchBar/SearchBar/SearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarApp.swift
+//  SearchBar
+//
+//  Created by Elaidzha Shchukin on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
