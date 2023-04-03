5a3f14ae52d8aa48717019c8dd1040cf019c7354
Elaidzha1940 <eli080293@gmail.com>
2023-04-03 13:31:46 +0700

    SearchBar

--- SearchBar/SearchBar/ContentView.swift
+++ SearchBar/SearchBar/ContentView.swift
@@ -1,23 +1,47 @@
+//  /*
 //
-//  ContentView.swift
-//  SearchBar
+//  Project: SearchBar
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 03.04.2023
 //
-//  Created by Elaidzha Shchukin on 03.04.2023.
+//  Status:  /  Decoreted
 //
+//  */
 
 import SwiftUI
 
-struct ContentView: View {
+// MARK: SearchBar | Поиск
+
+struct SearchBarComponent: View {
+    
+    @State var SearchText: String = ""
+    
     var body: some View {
         VStack {
-          Text("ddd")
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.white)
+                
+                TextField("Поиск", text: $SearchText)
+                    .foregroundColor(.gray)
+                
+            }
+            .frame(height: 4)
+            .font(.title3)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 13)
+                    .fill(Color.elements(.greySearch))
+            )
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct SearchBarComponent_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        self.devices{
+            SearchBarComponent()
+        }
     }
 }
